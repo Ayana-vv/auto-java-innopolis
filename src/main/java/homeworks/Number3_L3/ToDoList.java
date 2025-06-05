5cdb0cf2fc5259bd3d67031cb4dbf1ac0f4ae86b
Ayana-vv <ayana_will@mail.ru>
2025-06-05 03:44:57 +0300

    Initial commit

---
+++ src/main/java/homeworks/Number3_L3/ToDoList.java
@@ -0,0 +1,20 @@
+package homeworks.Number3_L3;
+
+public class ToDoList {
+    public static void main(String[] args) {
+        //массив строк
+        String[] todoList = new String[5];
+        todoList[0] = "проснуться";
+        todoList[1] = "умыться";
+        todoList[2] = "покушать";
+        todoList[3] = "посмотреть сериалы";
+        todoList[4] = "пойти по делам";
+
+        //массив чисел
+        float[] chisla = new float[3];
+        chisla[0] = 3.14159f;
+        chisla[1] = 2.71828f;
+        chisla[2] = 1;
+    }
+
+}
---
+++ src/main/java/org/example/Lesson4/Lesson4.java
@@ -0,0 +1,25 @@
+package org.example.Lesson4;
+
+public class Lesson4 {
+    public static void main(String[] args) {
+//        Track track = new Track(3, "Jake", "XO");
+//        System.out.println(track.author);
+
+        String human1 = "89995551122";
+        String human2 = "89554447788";
+        String human3 = "89990002233";
+        String human4 = "89995551122";
+
+        String[] humans = new String[4];
+        humans[0] = "89995551122";
+        humans[1] = "89554447788";
+        humans[2] = "89990002233";
+        humans[3] = "89995551122";
+
+        System.out.println(humans[0]);
+
+        int[] numbers = new int[100];
+        System.out.println(numbers[99]);
+
+    }
+}
